7102cbaead587aac5703ead51eb4242ae7cbe6ac
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-11 01:20:14 -0400

    make loader package-generic

--- loader.lisp
+++ loader.lisp
@@ -5,7 +5,7 @@
           (or (uiop:getenv "XDG_DATA_HOME")
               (merge-pathnames ".local/share/" (user-homedir-pathname)))))
        (source
-         (merge-pathnames "nyxt-starintel/src/starintel.lisp" data-home)))
+         (merge-pathnames "nyxt-guard/src/nyxt-guard.lisp" data-home)))
   (unless (probe-file source)
-    (error "nyxt-starintel source not found at ~a" source))
+    (error "nyxt-guard source not found at ~a" source))
   (nyxt::load-lisp source))
